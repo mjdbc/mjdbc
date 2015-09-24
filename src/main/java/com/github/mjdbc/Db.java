2d1f207930efe81190b20b60ab59f476a3444a4b
Mikhail Fursov <mike.fursov@gmail.com>
2015-09-23 18:21:54 -0700

    Javadoc update

--- src/main/java/com/github/mjdbc/Db.java
+++ src/main/java/com/github/mjdbc/Db.java
@@ -80,7 +80,8 @@ public interface Db {
 
 
     /**
-     * Returns per-method timers. Statistics is collected for all @Tx methods of attached Dbi interfaces
+     * Returns per-method timers.
+     * Statistics is collected for all @Tx methods of attached Dbi interfaces
      * and for all @Sql methods of attached query interfaces.
      *
      * @return per-method statistics. Note: This is direct access to the timers map. Timers are updated concurrently.
